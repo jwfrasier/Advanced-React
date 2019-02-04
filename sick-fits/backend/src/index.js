9ba2e28482d720714a3762a22141410c43d749fd
jwfrasier <frasier.joseph@gmail.com>
2019-02-04 09:56:20 -0600

    setup for db

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -2,3 +2,16 @@ require("dotenv").config({ path: "variables.env" });
 const createServer = require("createServer");
 const db = require("./db");
 const server = createServer();
+
+// todo use express middleware to handle cookies (JWT)
+
+//TODO use express middleware to populate current users
+
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin.process.env
+        }
+    }
+)
