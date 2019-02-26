f80436b18603c5fe269f3e0f8a6aa13130c786b2
jwfrasier <frasier.joseph@gmail.com>
2019-02-26 15:11:10 -0600

    creating queries

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutations = {};
+const Mutations = {
+  // createDog(parent, arguments, context, info) {
+  //   //create a dog here!
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: arguments.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  //   // console.log(arguments);
+  // }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  // this is where your database calls would go
+  //   dogs(parent, arguments, context, info) {
+  //     global.dogs = global.dogs || [];
+  //     return global.dogs;
+  //   }
+};
 
 module.exports = Query;
