297a14fdb41dd05ac7f47aa8150a8909f473e2cd
jwfrasier <frasier.joseph@gmail.com>
2019-03-05 16:42:05 -0600

    items appearing on page.  apollo linked to react

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,18 +1,20 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from "next-with-apollo"; // gives us high order component that exposes apollo client via a prop
+
+import ApolloClient from "apollo-boost"; // import apollo client from apollo boost
+import { endpoint } from "../config"; // endpoint that shows us our yoga api
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
     request: operation => {
+      // when you make a request, the logged in cookies in the browser come in
       operation.setContext({
         fetchOptions: {
-          credentials: 'include',
+          credentials: "include"
         },
-        headers,
+        headers
       });
-    },
+    }
   });
 }
 
