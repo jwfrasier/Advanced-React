75b996e068600c407e1e3cef7ad99ab5c990066b
jwfrasier <frasier.joseph@gmail.com>
2019-03-12 15:22:39 -0500

    changes for the form

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -6,15 +6,28 @@ import formatMoney from "../lib/formatMoney";
 
 class CreateItem extends Component {
   state = {
-    description: "",
-    title: "",
-    largeImage: "",
-    image: "",
-    price: 0
+    description: "Nice Nice Nice",
+    title: "Big Nice",
+    largeImage: "big-doge.jpg",
+    image: "smalldoge.jpg",
+    price: 100
   };
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    console.log({ name, type, value });
+    this.setState({ [name]: val });
+  };
+
   render() {
     return (
-      <Form>
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(this.state);
+        }}
+      >
         <fieldset>
           <label htmlFor="title">
             Title
@@ -25,8 +38,34 @@ class CreateItem extends Component {
               placeholder="Title"
               required
               value={this.state.title}
+              onChange={this.handleChange}
             />
           </label>
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="description">
+            Description
+            <input
+              id="description"
+              name="description"
+              placeholder="Enter A Description"
+              required
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
         </fieldset>
       </Form>
     );
